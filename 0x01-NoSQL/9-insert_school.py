d748c6709a9040af79fb9a69dfd64847e74c750e
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-07-15 12:56:43 -0700

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    return mongo_collection.insert(kwargs)
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
