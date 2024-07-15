a92cccae542bf9ed0ac14650cbdf8983f4211568
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-07-15 12:36:33 -0700

    task 8

--- 0x01-NoSQL/8-main.py
+++ 0x01-NoSQL/8-main.py
@@ -6,8 +6,8 @@ list_all = __import__('8-all').list_all
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
-    print(school_collection)
+    # print(school_collection)
     schools = list_all(school_collection)
-    print(schools)
-    # for school in schools:
-    # print("[{}] {}".format(school.get('_id'), school.get('name')))
+    # print(schools)
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
